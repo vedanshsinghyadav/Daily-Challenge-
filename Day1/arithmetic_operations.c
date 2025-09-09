d3790aae45ae7d480c2a37b4ae75425d5a7fe025
vedanshsinghyadav <vedanshsinghyadav05@gmail.com>
2025-09-09 18:50:27 +0530

    daily challenge

--- Day1/arithmetic_operations.c
+++ Day1/arithmetic_operations.c
@@ -1,4 +1,4 @@
-// Write a program to input two numbers and display their sum, difference, product, and quotient.
+// Write a program to input two numbers and display their sum, difference, product, and quotient
 
 
 #include <stdio.h>
