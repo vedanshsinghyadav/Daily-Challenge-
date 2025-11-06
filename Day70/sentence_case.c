da73647ca52b0cdebf29aa776dafb3f0a3309fe8
vedanshsinghyadav <vedanshsinghyadav05@gmail.com>
2025-11-06 23:03:06 +0530

    Update sentence_case.c code and fix the error  in 33 line

--- Day70/sentence_case.c
+++ Day70/sentence_case.c
@@ -3,33 +3,33 @@
 
 
 
-#include <stdio.h>   // For input/output functions like printf, fgets
-#include <string.h>  // For string functions like strcspn
-#include <ctype.h>   // For character functions like toupper, tolower
+#include <stdio.h>   
+#include <string.h>  
+#include <ctype.h>   
 
-// Function to convert string to title case (first letter of each word capitalized)
+
 void toSentenceCase(char *str) {
-    int i = 0;           // Counter variable to traverse through the string
-    int newWord = 1;     // Flag: 1 means next letter is start of a new word, 0 means middle of word
+    int i = 0;           
+   int newWord = 1;     
     
-    // Loop through each character until we reach end of string (null character '\0')
+    
     while (str[i] != '\0') {
         
-        // Check if current character is a space
+        
         if (str[i] == ' ') {
-            // When we find a space, the next non-space character will be start of new word
+            
             newWord = 1;
         } 
-        // Check if we're at the start of a new word
+        
         else if (newWord) {
-            // Convert this character to uppercase (capital letter)
+           
             str[i] = toupper(str[i]);
-            // Now we're inside the word, so set flag to 0
+            
             newWord = 0;
         } 
-        // If not a space and not start of word, then it's middle of a word
+        
         else {
-            // Convert this character to lowercase (small letter)
+            
             str[i] = tolower(str[i]);
         }
         
