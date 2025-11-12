a7df4215fe3b9d117d689a844190d6c507d5163d
vedanshsinghyadav <vedanshsinghyadav05@gmail.com>
2025-11-12 23:37:42 +0530

    Added C program to count characters, words, and lines in a text file

---
+++ Day73/count_text_stats.c
@@ -0,0 +1,50 @@
+/*Read a text file and count the total number of characters, words, and lines.
+ A word is defined as a sequence of non-space characters separated by spaces or newlines*/
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char filename[100], ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    // File name input
+    printf("Enter file name: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (isspace(ch))
+            inWord = 0;
+        else if (inWord == 0) {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(fp);
+
+    // If file doesn’t end with newline, still count last line
+    if (characters > 0 && ch != '\n')
+        lines++;
+
+    printf("Characters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Lines: %d\n", lines);
+
+    return 0;
+}
