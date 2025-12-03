9468581fb9128706282ac4daf6882d53fa7157b7
vedanshsinghyadav <vedanshsinghyadav05@gmail.com>
2025-12-03 11:29:55 +0530

    code

--- Day73/count_text_stats.c
+++ Day73/count_text_stats.c
@@ -1,65 +1,46 @@
-/*Read a text file and count the total number of characters, words, and lines.
- A word is defined as a sequence of non-space characters separated by spaces or newlines*/
-
+// /*Read a text file and count the total number of characters, words, and lines.
+//  A word is defined as a sequence of non-space characters separated by spaces or newlines*/
 
 #include <stdio.h>
 #include <ctype.h>
 
 int main() {
-    FILE *file;
-    char filename[100];
-    char ch;
+    FILE *fp;
+    char filename[100], ch;
     int characters = 0, words = 0, lines = 0;
-    int in_word = 0;  // Flag to track if we're currently in a word
-    
-    // Get filename from user
-    printf("Enter filename: ");
+    int inWord = 0;
+
+    printf("Enter file name: ");
     scanf("%s", filename);
-    
-    // Open file
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error: Could not open file %s\n", filename);
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error opening file.\n");
         return 1;
     }
-    
-    // Read file character by character
-    while ((ch = fgetc(file)) != EOF) {
+
+    while ((ch = fgetc(fp)) != EOF) {
         characters++;
-        
-        // Count lines
-        if (ch == '\n') {
+
+        if (ch == '\n')
             lines++;
-            if (in_word) {
-                in_word = 0;
-            }
-        }
-        // Count words (non-space character starts a word)
-        else if (ch == ' ' || ch == '\t') {
-            if (in_word) {
-                in_word = 0;
-            }
-        }
-        else {
-            if (!in_word) {
+
+        if (!isspace(ch)) {
+            if (inWord == 0) {
                 words++;
-                in_word = 1;
+                inWord = 1;
             }
+        } else {
+            inWord = 0;
         }
     }
-    
-    // If file doesn't end with newline but has content, count it as a line
-    if (characters > 0 && ch != '\n') {
-        lines++;
-    }
-    
-    // Close file
-    fclose(file);
-    
-    // Display results
-    printf("\nCharacters: %d\n", characters);
-    printf("Words: %d\n", words);
-    printf("Lines: %d\n", lines);
-    
+
+    fclose(fp);
+
+    printf("Total Characters: %d\n", characters);
+    printf("Total Words: %d\n", words);
+    printf("Total Lines: %d\n", lines);
+
     return 0;
 }
